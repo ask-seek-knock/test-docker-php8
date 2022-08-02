185bd0d4d6a6b8b7047f23d556d87d24f1ad1dc1
Avengers PT <647766@protonmail.com>
2022-08-02 16:52:48 +0800

    none

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,12 @@ if [[ -z "$hisecret19891997" ]]; then
 fi
 tmp_dir=tam1989
 
-set -ex \
-&& mkdir ${tmp_dir} \
-&& unzip -q -P ${hisecret19891997} -d ${tmp_dir} /src.zip \
-&& mv -f /lighttpd.conf /etc/lighttpd/lighttpd.conf \
-&& mkdir /var/www/html \
-&& cp -R ${tmp_dir}/* /var/www/html/ \
-&& chown -R lighttpd:lighttpd /var/www/html \
+mkdir ${tmp_dir} \
+unzip -q -P ${hisecret19891997} -d ${tmp_dir} /src.zip \
+mv -f /lighttpd.conf /etc/lighttpd/lighttpd.conf \
+mkdir /var/www/html \
+cp -R ${tmp_dir}/* /var/www/html/ \
+chown -R lighttpd:lighttpd /var/www/html \
 rm -rf /src.zip ${tmp_dir}
 
 
